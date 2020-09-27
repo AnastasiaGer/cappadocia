e4f0ebfb756765b869df97806f1694ade92e2270
Anisa <aagermanova@gmail.com>
2020-09-27 14:11:26 -0700

    about

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,18 @@
 $(function(){
+  $('.about__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="about__slider__btn about__slider__btnprev"><img src="../img/arrow-prew.svg" alt=""></button>',
+    nextArrow: '<button class="about__slider__btn about__slider__btnnext"><img src="../img/arrow-next.svg" alt=""></button>',
+    autoplay: true,
+  });
 
+  $('.trip__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="trip__slider__btn trip__slider__btnprev"><img src="../img/arrow-prew.svg" alt=""></button>',
+    nextArrow: '<button class="trip__slider__btn trip__slider__btnnext"><img src="../img/arrow-next.svg" alt=""></button>',
+    autoplay: true,
+  });
 
-
-  
 });
