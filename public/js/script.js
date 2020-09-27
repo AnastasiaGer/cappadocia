4e752b71463e31897cd78349e8a39d844d11931e
Anisa <aagermanova@gmail.com>
2020-09-27 14:30:15 -0700

    trip

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,7 @@ $(function(){
     prevArrow: '<button class="about__slider__btn about__slider__btnprev"><img src="../img/arrow-prew.svg" alt=""></button>',
     nextArrow: '<button class="about__slider__btn about__slider__btnnext"><img src="../img/arrow-next.svg" alt=""></button>',
     autoplay: true,
+    fade: true,
   });
 
   $('.trip__slider').slick({
@@ -13,6 +14,7 @@ $(function(){
     prevArrow: '<button class="trip__slider__btn trip__slider__btnprev"><img src="../img/arrow-prew.svg" alt=""></button>',
     nextArrow: '<button class="trip__slider__btn trip__slider__btnnext"><img src="../img/arrow-next.svg" alt=""></button>',
     autoplay: true,
+    fade: true,
   });
 
 });
