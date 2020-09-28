26d33da809d21b0edadc98d61603360a68a74721
Anisa <aagermanova@gmail.com>
2020-09-28 11:53:14 -0700

    scroll

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   $('.about__slider, .trip__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -6,6 +6,20 @@ $(function(){
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="../img/arrow-next.svg" alt=""></button>',
     autoplay: true,
     fade: true,
+    responsive: [{
+      breakpoint: 601,
+      settings: {
+        arrows: false
+      }
+    }]
   });
 
+  $('.menu').on('click', 'a', function (event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+    top = $(id).offset().top;
+
+    $('body,html').animate({scrollTop: top}, 1500);
+
+  });
 });
