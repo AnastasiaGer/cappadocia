9295a9cae1d9c546a5c759f8b301a206f76c48fa
Anisa <aagermanova@gmail.com>
2020-09-27 21:06:44 -0700

    fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,9 @@
 $(function(){
-  $('.about__slider').slick({
+  $('.about__slider, .trip__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: '<button class="about__slider__btn about__slider__btnprev"><img src="../img/arrow-prew.svg" alt=""></button>',
-    nextArrow: '<button class="about__slider__btn about__slider__btnnext"><img src="../img/arrow-next.svg" alt=""></button>',
-    autoplay: true,
-    fade: true,
-  });
-
-  $('.trip__slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: '<button class="trip__slider__btn trip__slider__btnprev"><img src="../img/arrow-prew.svg" alt=""></button>',
-    nextArrow: '<button class="trip__slider__btn trip__slider__btnnext"><img src="../img/arrow-next.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="../img/arrow-prew.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="../img/arrow-next.svg" alt=""></button>',
     autoplay: true,
     fade: true,
   });
